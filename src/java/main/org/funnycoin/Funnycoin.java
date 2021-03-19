4552b77c9d45a76b307c42e090cd49d8de1c4402
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-19 15:52:54 -0400

    Update Funnycoin.java

--- src/java/main/org/funnycoin/Funnycoin.java
+++ src/java/main/org/funnycoin/Funnycoin.java
@@ -2,5 +2,7 @@ package org.funnycoin;
 
 public class Funnycoin {
   public static void main(String[] args) {
+    File blockchain = new File("blocks.txt");
+    File peers = new File("peers.txt");
   }
 }
