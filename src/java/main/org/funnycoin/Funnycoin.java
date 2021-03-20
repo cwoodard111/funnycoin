f9580e165e2e42a7a53d7c484de42650a0fa3aae
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-20 14:27:42 -0400

    Update Funnycoin.java

--- src/java/main/org/funnycoin/Funnycoin.java
+++ src/java/main/org/funnycoin/Funnycoin.java
@@ -5,15 +5,21 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.funnycoin.blocks.Block;
 import org.funnycoin.transactions.Transaction;
+import org.funnycoin.wallet.Wallet;
 
 import java.io.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Funnycoin {
 
-    Funnycoin(NodeType type) throws IOException {
+    Funnycoin(NodeType type) throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
         if(type == NodeType.MINER) {
             File blockChainFile = new File("blockchain.json");
             BufferedReader chainReader = new BufferedReader(new FileReader(blockChainFile));
@@ -23,27 +29,33 @@ public class Funnycoin {
                 chainBuilder.append(line);
             }
             String json = chainBuilder.toString();
+            Wallet wallet1 = new Wallet();
+            Wallet wallet2 = new Wallet();
+
             if(json.length() < 1) {
-                System.out.println("genesis block time: TESTING WITH PUBLIC KEYS THAT DONT EXIST");
-                Transaction transaction = new Transaction("blaze","blaze2",50f);
-                List<Transaction> transactions = new ArrayList<>();
-                transactions.add(transaction);
-                Block genesisBlock = new Block(transactions);
-                FunnycoinCache.blockChain.add(genesisBlock);
-                Gson gson = new Gson();
-                String newJson = gson.toJson(FunnycoinCache.blockChain);
-                BufferedWriter writer = new BufferedWriter(new FileWriter(new File("blockchain.json")));
-                writer.write(newJson);
-                writer.close();
+                Wallet genesisWallet = new Wallet();
+                genesisWallet.sendGenesis(wallet2.publicKey,50.f);
             } else {
                 JsonParser chainParser = new JsonParser();
                 JsonArray blockChainArray = (JsonArray) chainParser.parse(json);
                 Gson gson = new Gson();
                 Block[] blockChain = gson.fromJson(blockChainArray, Block[].class);
                 FunnycoinCache.blockChain = Arrays.asList(blockChain);
+            }
                 Block currentBlock = FunnycoinCache.blockChain.get(FunnycoinCache.blockChain.size() - 1);
-                System.out.println("waiting for block");
-                System.out.println("owner bal: " + getBalanceFromChain("blaze2"));
+
+                System.out.println(wallet1.getBase64Key(wallet1.publicKey) + " wallet2:" + wallet2.getBase64Key(wallet2.publicKey));
+                while(true) {
+                    Scanner scanner = new Scanner(System.in);
+                    String linew;
+                    if((linew = scanner.nextLine()).toLowerCase().contains("send")) {
+                        String[] args = linew.split(" ");
+                        String reciever = args[1];
+                        float amount = Float.parseFloat(args[2]);
+                        wallet2.send(reciever,amount);
+                    }
+
+                    System.out.println(wallet1.getBalanceFromChain(wallet1.publicKey) + " " + wallet2.getBalanceFromChain(wallet2.publicKey));
             }
         }
     }
@@ -51,17 +63,16 @@ public class Funnycoin {
         if(type == NodeType.WALLET) {
             loadChainWallet();
             float balance = getBalanceFromChain(publicKey);
-            System.out.println("BALANCE: " + balance);
         }
     }
 
     private float getBalanceFromMiningNodes(String publicKey) {
-        //TODO add p2p network to give the client it's balance but for now we're just going to require everyone to download the chain.
+        //TODO add p2p network to give the client it's balance but for now we're just going to require everyone to download the chain for testing.
         return 0.0f;
     }
-
     private void loadChainWallet() throws IOException {
         File blockChainFile = new File("blockchain.json");
+
         BufferedReader chainReader = new BufferedReader(new FileReader(blockChainFile));
         StringBuilder chainBuilder = new StringBuilder();
         String line;
@@ -75,6 +86,7 @@ public class Funnycoin {
         Block[] blockChain = gson.fromJson(blockChainArray, Block[].class);
         FunnycoinCache.blockChain = Arrays.asList(blockChain);
     }
+
     private float getBalanceFromChain(String publicKey) {
         float balance = 0.0f;
         for(Block block : FunnycoinCache.blockChain) {
@@ -87,7 +99,8 @@ public class Funnycoin {
         return balance;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
         if(args.length == 2) {
             if(args[0].toLowerCase().equals("miner")) {
                 new Funnycoin(NodeType.MINER);
@@ -96,6 +109,11 @@ public class Funnycoin {
             }
         }
     }
+
+
+
+
+
     public enum NodeType {
         MINER,
         VOTER,
