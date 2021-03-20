9ade7935c61a3b2a46b6cf87568266175ad999c3
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-20 10:23:26 -0400

    Update Block.java

--- src/java/main/org/funnycoin/blocks/Block.java
+++ src/java/main/org/funnycoin/blocks/Block.java
@@ -1,8 +1,18 @@
 package org.funnycoin.blocks;
 
+import org.funnycoin.transactions.Transaction;
+
+import java.util.List;
+
 public class Block {
-  List<Transaction> transactions;
-  public Block(List<Transaction> transactions) {
-    this.transactions = transactions;
-  }
+    List<Transaction> transactions;
+
+    public Block(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
 }
