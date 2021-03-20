b06e093b7fe49fc302c9b7248e8c4dc5e1aac21c
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-20 19:12:08 -0400

    Update Transaction.java

--- src/java/main/org/funnycoin/transactions/Transaction.java
+++ src/java/main/org/funnycoin/transactions/Transaction.java
@@ -1,10 +1,13 @@
 package org.funnycoin.transactions;
 
 
+import java.security.MessageDigest;
+
 public class Transaction {
     String ownerKey;
     String outputKey;
     float amount;
+    public String transactionId;
 
     public Transaction(String ownerKey, String outputKey, float amount) {
         this.outputKey = outputKey;
@@ -24,4 +27,26 @@ public class Transaction {
         return amount;
     }
 
+    private String getHash() {
+        return applySha256(ownerKey + outputKey);
+    }
+
+    public String applySha256(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
