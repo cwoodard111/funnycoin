f65d0170eb839637b301c0a31bf70af1d486eab3
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-20 14:28:02 -0400

    Create Wallet.java

---
+++ src/java/main/org/funnycoin/wallet/Wallet.java
@@ -0,0 +1,94 @@
+package org.funnycoin.wallet;
+
+import com.google.gson.Gson;
+import org.funnycoin.FunnycoinCache;
+import org.funnycoin.blocks.Block;
+import org.funnycoin.transactions.Transaction;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public Wallet() throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        generateKeyPair();
+    }
+
+
+    public void generateKeyPair() throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+        ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+        keyGen.initialize(ecSpec,random);
+        KeyPair pair = keyGen.generateKeyPair();
+        privateKey = pair.getPrivate();
+        publicKey = pair.getPublic();
+    }
+
+
+    public void send(String reciever,float amount) throws IOException {
+        if(getBalanceFromChain(publicKey) < amount) {
+            System.out.println("insufficient funds");
+            return;
+        }
+
+        Transaction transaction = new Transaction(getBase64Key(publicKey),reciever,amount);
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+        Block currentBlock = FunnycoinCache.blockChain.get(FunnycoinCache.blockChain.size() - 1);
+        if(currentBlock.isFull()) {
+            Block block = new Block(transactions);
+            FunnycoinCache.blockChain.add(block);
+        } else {
+            currentBlock.getTransactions().add(transaction);
+            Gson gson = new Gson();
+            String newJson = gson.toJson(FunnycoinCache.blockChain);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("blockchain.json")));
+            writer.write(newJson);
+            writer.close();
+        }
+    }
+
+        public String getBase64Key(PublicKey key) {
+            return Base64.getEncoder().encodeToString(key.getEncoded());
+        }
+
+
+        public void sendGenesis(PublicKey reciever, float amount) throws IOException {
+
+            Transaction transaction = new Transaction(getBase64Key(publicKey),getBase64Key(reciever),amount);
+            List<Transaction> transactions = new ArrayList<>();
+            transactions.add(transaction);
+            Block genesisBlock = new Block(transactions);
+            FunnycoinCache.blockChain.add(genesisBlock);
+            Gson gson = new Gson();
+            String newJson = gson.toJson(FunnycoinCache.blockChain);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("blockchain.json")));
+            writer.write(newJson);
+            writer.close();
+    }
+
+    public float getBalanceFromChain(PublicKey publicKey) {
+        float balance = 0.0f;
+        for(Block block : FunnycoinCache.blockChain) {
+            for(Transaction transaction : block.getTransactions()) {
+                if((transaction.getOutputKey().contains(getBase64Key(publicKey)))) {
+                    balance += transaction.getAmount();
+                } else if(transaction.getOwnerKey().contains(getBase64Key(publicKey))) {
+                    balance -= transaction.getAmount();
+                }
+            }
+        }
+        return balance;
+    }
+
+}
