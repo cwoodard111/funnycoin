13032095eef3236a1123a19719bcf53f057b9728
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-20 10:22:55 -0400

    Update FunnycoinCache.java

--- src/java/main/org/funnycoin/FunnycoinCache.java
+++ src/java/main/org/funnycoin/FunnycoinCache.java
@@ -1,16 +1,22 @@
 package org.funnycoin;
 
+import org.funnycoin.blocks.Block;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FunnycoinCache {
-  public static List<Block> blockChain = new ArrayList<Block>();
-  
-  public static int getCirculation() {
-    return blockChain.size();
-  }
-  
-  public static int getReward() {
-    return blockChain.size() 
-  }
-  
-  public static int getInputReward() {
-    return blockChain.size() < 10000 ? 50 : 40;
-  }
+    public static List<Block> blockChain = new ArrayList<>();
+
+    public static int getCirculation() {
+        return blockChain.size();
+    }
+
+    public static int getReward() {
+        return 50;
+    }
+
+    public static int getInputReward() {
+        return blockChain.size() < 10000 ? 50 : 40;
+    }
+}
