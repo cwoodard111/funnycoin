9c8e119c5730d5bde6d470e21eba784e1fc48bdd
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-21 21:00:54 -0400

    Update SignageUtils.java

--- src/java/main/org/funnycoin/wallet/SignageUtils.java
+++ src/java/main/org/funnycoin/wallet/SignageUtils.java
@@ -1,9 +1,7 @@
 package org.funnycoin.wallet;
 
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
@@ -21,13 +19,28 @@ public class SignageUtils {
     }
 
 
-    public static PublicKey getKey(String key){
-        try{
-            byte[] byteKey = Base64.getDecoder().decode(key.getBytes());
-            X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
-            KeyFactory kf = KeyFactory.getInstance("RSA");
+    public static PrivateKey getPrivateKey(String key){
+        try {
+            byte[] keyBytes = Base64.getDecoder().decode(key.getBytes("utf-8"));
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory fact = KeyFactory.getInstance("RSA");
+            return fact.generatePrivate(keySpec);
 
-            return kf.generatePublic(X509publicKey);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static PublicKey getPublicKey(String key){
+        try{
+            byte[] keyBytes = Base64.getDecoder().decode(key.getBytes("utf-8"));
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return keyFactory.generatePublic(spec);
+
         }
         catch(Exception e){
             e.printStackTrace();
@@ -35,4 +48,25 @@ public class SignageUtils {
 
         return null;
     }
+
+    public static String applySha256(String input){
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hash = digest.digest(input.getBytes("UTF-8"));
+
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
