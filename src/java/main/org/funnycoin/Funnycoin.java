3dcbf25ab2069f4a4dba0cf298c0358f984db557
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-21 21:01:31 -0400

    Update Funnycoin.java

--- src/java/main/org/funnycoin/Funnycoin.java
+++ src/java/main/org/funnycoin/Funnycoin.java
@@ -5,13 +5,12 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.funnycoin.blocks.Block;
 import org.funnycoin.p2p.NetworkManager;
+import org.funnycoin.p2p.server.PeerServer;
 import org.funnycoin.transactions.Transaction;
+import org.funnycoin.wallet.SignageUtils;
 import org.funnycoin.wallet.Wallet;
 
 import java.io.*;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.Security;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,19 +18,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Funnycoin {
-    
-    
-    
-    /**
-    * big disclaimer: most of this code is for running file node which isnt going to happen so i have to add a whole ton more stuff for it to ACTUALLY work with p2p, same with Wallet.java
-    */
-    
-    
-    
-   
 
     Funnycoin(NodeType type) throws Exception {
         if(type == NodeType.MINER) {
+            System.out.println("loaded mining system ");
             File blockChainFile = new File("blockchain.json");
             BufferedReader chainReader = new BufferedReader(new FileReader(blockChainFile));
             StringBuilder chainBuilder = new StringBuilder();
@@ -40,12 +30,19 @@ public class Funnycoin {
                 chainBuilder.append(line);
             }
             String json = chainBuilder.toString();
-            Wallet wallet1 = new Wallet();
-            Wallet wallet2 = new Wallet();
-
+            /**
+             *
+             */
             if(json.length() < 1) {
-                Wallet genesisWallet = new Wallet();
-                genesisWallet.sendGenesis(wallet2.publicKey,50.f);
+                Block genesis = new Block("first block ever");
+                ArrayList<Transaction> transactionList = new ArrayList<>();
+                transactionList.add(new Transaction("coinbase","MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAK4cjvdZ1XfjAcK2s3ysMrSiVEIi9hNl3il79DAFmKg3tyet7l5i8gRy2j2qvfe8KkQGBO9EAOWjfrKWazDKLqMCAwEAAQ==",10000f,"null"));
+                genesis.transactions = transactionList;
+                FunnycoinCache.blockChain.add(genesis);
+                BufferedWriter chainWriter = new BufferedWriter(new FileWriter(blockChainFile));
+                Gson gson = new Gson();
+                chainWriter.write(gson.toJson(FunnycoinCache.blockChain));
+                chainWriter.close();
             } else {
                 JsonParser chainParser = new JsonParser();
                 JsonArray blockChainArray = (JsonArray) chainParser.parse(json);
@@ -53,18 +50,65 @@ public class Funnycoin {
                 Block[] blockChain = gson.fromJson(blockChainArray, Block[].class);
                 FunnycoinCache.blockChain = Arrays.asList(blockChain);
             }
-            NetworkManager networkManager = new NetworkManager();
+            System.out.println("Loading peerserver:");
+            PeerServer server = FunnycoinCache.peerServer;
+            System.out.println("we genned it i think? thatll be funny");
+            server.initListeners();
+            Thread runner = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        NetworkManager manager = new NetworkManager();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            System.out.println("ok and?");
 
 
 
             //TODO: CHECK IF BLOCK VALID ON SEND WITH P2P NETWORK
 
-        }
-    }
-    Funnycoin(NodeType type, String publicKey) throws IOException {
-        if(type == NodeType.WALLET) {
-            loadChainWallet();
-            float balance = getBalanceFromChain(publicKey);
+        } else if(type == NodeType.WALLET) {
+            System.out.println("pre nw");
+            NetworkManager manager = new NetworkManager();
+            System.out.println("tetatst1243ing");
+            Wallet myWallet = new Wallet();
+            System.out.println("hallo wallet");
+            while(true) {
+                Scanner scanner = new Scanner(System.in);
+                String cmd = scanner.nextLine();
+                String[] args = cmd.split(" ");
+                for(String arg : args) {
+                    System.out.println(arg);
+                }
+                String command = args[0];
+
+                if(command.toLowerCase().contains("send")) {
+                    System.out.println("yeasss");
+                    String ownerKey = myWallet.getBase64Key(myWallet.publicKey);
+                    String outputKey = args[1];
+                    float amount = Float.parseFloat(args[2]);
+
+                    /**
+                     * Setup transaction json for sending to remote nodes :)
+                     */
+                    JsonObject transactionData = new JsonObject();
+                    transactionData.addProperty("event","transaction");
+                    transactionData.addProperty("ownerKey",ownerKey);
+                    transactionData.addProperty("recipientKey",outputKey);
+                    transactionData.addProperty("amount",amount);
+                    System.out.println(transactionData.toString());
+                    // creating signature now hold up lemme find the method
+                    System.out.println(SignageUtils.applySha256(ownerKey + outputKey + amount));
+                    String signatureData = SignageUtils.sign(SignageUtils.applySha256(ownerKey + outputKey + amount), myWallet.privateKey);
+                    System.out.println("signed: " + signatureData);
+                    transactionData.addProperty("signatureData",transactionData.toString());
+                    manager.broadcast(transactionData.toString());
+                }
+
+            }
         }
     }
 
@@ -111,7 +155,7 @@ public class Funnycoin {
             if(args[0].toLowerCase().equals("miner")) {
                 new Funnycoin(NodeType.MINER);
             } else if(args[0].toLowerCase().contains("wallet")) {
-                new Funnycoin(NodeType.WALLET,args[1]);
+                new Funnycoin(NodeType.WALLET);
             }
         }
     }
