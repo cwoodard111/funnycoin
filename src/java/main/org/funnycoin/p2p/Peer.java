031605d9fe7e53030c08d8e01cd83ddbc08e6ec5
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-03-23 17:32:08 -0400

    Update Peer.java

--- src/java/main/org/funnycoin/p2p/Peer.java
+++ src/java/main/org/funnycoin/p2p/Peer.java
@@ -1,21 +1,19 @@
 package org.funnycoin.p2p;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.funnycoin.FunnycoinCache;
 import org.funnycoin.blocks.Block;
-import org.funnycoin.p2p.server.PeerServer;
 import org.funnycoin.transactions.Transaction;
 import org.funnycoin.wallet.SignageUtils;
-import org.funnycoin.wallet.Wallet;
 
+import static org.funnycoin.p2p.RequestParams.*;
 import java.io.*;
 import java.net.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Peer {
@@ -67,6 +65,51 @@ public class Peer {
         return isAlive;
     }
 
+    private boolean isChainValid(List<Block> blockChain) throws Exception {
+        for(int i = 0; i < blockChain.size(); i++) {
+            String difficultyTarget = new String(new char[FunnycoinCache.getDifficulty()]).replace('\0','0');
+            Block currentBlock = blockChain.get(i);
+            if(blockChain.size() >= 1) {
+                Block previousBlock = blockChain.get(i - 1);
+                /**
+                 * Performing checks on the temporary blockchain to see if it's valid.
+                 */
+                if(!currentBlock.hash.equals(currentBlock.getHash())) {
+                    System.out.println("The hash of the block is not equal to the calculated value.");
+                    return false;
+                }
+                if(!previousBlock.hash.equals(currentBlock.getHash())) {
+                    System.out.println("The hash of the previous block is not equal to the calculated value.");
+                    return false;
+                }
+                if(!currentBlock.hash.substring(0,FunnycoinCache.getDifficulty()).equals(difficultyTarget)) {
+                    System.out.println("The block does not have a Proof-Of-Work attached to it.");
+                    return false;
+                }
+                /**
+                 * Checking transactions for validity or errors.
+                 */
+                for(int j = 0; j < currentBlock.getTransactions().size(); j++) {
+                    Transaction currentTransaction = currentBlock.transactions.get(j);
+
+                    if(!currentTransaction.verify(currentTransaction.getHash(),currentTransaction.signature, SignageUtils.getPublicKey(currentTransaction.getOwnerKey()))) {
+                        if(currentTransaction.getOwnerKey().toLowerCase().equals("coinbase")) {
+                            return true;
+                        }
+                    }
+                }
+
+
+            } else {
+                /**
+                 * We are going to just say it's correct because it's at genesis.
+                 */
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void startListener() throws Exception {
         System.out.println("starting listener");
             while(true) {
@@ -74,6 +117,30 @@ public class Peer {
                 String tmp;
                 if((tmp = reader.readLine()) != null) {
                     System.out.println(tmp);
+                    JsonObject eventObject = JsonParser.parseString(tmp).getAsJsonObject();
+                    String event = eventObject.get("event").getAsString();
+                    if(event.toLowerCase().contains("blocksAfter")) {
+                        int height = eventObject.get("startingHeight").getAsInt();
+                        if(requestingBlocks) if(blockHeight == height) {
+                            Gson gson = new Gson();
+                            List<Block> tempChain = new ArrayList<>(FunnycoinCache.blockChain);
+                            JsonArray blockArray = eventObject.getAsJsonArray("blocks");
+                            Block[] blocksArray = gson.fromJson(blockArray, Block[].class);
+                            List<Block> incomingBlocks = Arrays.asList(blocksArray);
+                            for(Block b : incomingBlocks) {
+                                tempChain.add(b);
+                            }
+
+                            if(isChainValid(tempChain)) {
+                                FunnycoinCache.blockChain = tempChain;
+                                FunnycoinCache.syncBlockchainFile();
+                            } else {
+                                System.out.println("Denied blockchain, invalid.");
+                            }
+                            blockHeight = 0;
+                            requestingBlocks = false;
+                        }
+                    }
                 }
             }
         }
