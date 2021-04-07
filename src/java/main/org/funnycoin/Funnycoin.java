42596e86930bc56fe1ed48acdb9e2dca2ef07503
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-04-07 17:37:47 -0400

    Update Funnycoin.java

--- src/java/main/org/funnycoin/Funnycoin.java
+++ src/java/main/org/funnycoin/Funnycoin.java
@@ -1,146 +1,112 @@
 package org.funnycoin;
 
+import com.codebrig.beam.messages.BeamMessage;
+import com.dosse.upnp.UPnP;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.funnycoin.blocks.Block;
-import org.funnycoin.p2p.NetworkManager;
+import org.funnycoin.p2p.Peer;
 import org.funnycoin.transactions.Transaction;
-import org.funnycoin.wallet.SignageUtils;
-import org.funnycoin.wallet.Wallet;
 
-import java.io.IOException;
+import java.io.*;
 import java.security.Security;
-import java.util.List;
-import java.util.Scanner;
 
 import static org.funnycoin.FunnycoinCache.*;
 import static org.funnycoin.p2p.RequestParams.*;
 
 public class Funnycoin {
-    private int difficulty = 7;
+    private int difficulty = 6;
 
-    private void getBlocksAfter(int height) throws InterruptedException {
-        JsonObject requestObject = new JsonObject();
-        requestObject.addProperty("event","getBlocksAfter");
-        requestObject.addProperty("startingHeight",height);
-        blockHeight = height;
-        requestingBlocks = true;
-        final int blockChainHeight = getCurrentBlock().height;
-        while(true) {
-            if (gatheredBlocks.size() < 1) {
-                for (List<Block> blocks : gatheredBlocks) {
-                    if(blocks.size() > blockChainHeight) {
-                        System.out.println("Blockchain size is greater than the old one. we will use it!");
-                        for(Block block : blocks) {
-                            blockChain.add(block);
-                        }
-                    }
-                }
-                break;
-            }
-        }
-    }
 
     private void mine() throws IOException {
         Block mine = FunnycoinCache.getNextBlock();
         mine.transactions.add(new Transaction("coinbase", wallet.getBase64Key(wallet.publicKey),50.f,"null"));
-        mine.mine(getDifficulty());
-        Gson gson = new Gson();
-        peerServer.broadcast(gson.toJson(mine));
-        blockChain.add(mine);
-        syncBlockchainFile();
+        if(mine.mine(getDifficulty())) {
+            Gson gson = new Gson();
+            String json = gson.toJson(mine);
+            peerServer.broadcast(json, "newBlock");
+            blockChain.add(mine);
+            syncBlockchainFile();
+            interrupted = false;
+        }
+    }
+
+    private void loadConfig() {
+        try {
+            final File config = new File("config.json");
+            final StringBuilder builder = new StringBuilder();
+            final BufferedReader reader = new BufferedReader(new FileReader(config));
+            String tmp;
+            while((tmp = reader.readLine()) != null) builder.append(tmp);
+            JsonParser parser = new JsonParser();
+            JsonObject obj = (JsonObject) parser.parse(builder.toString());
+            int port = obj.get("port").getAsInt();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     Funnycoin(NodeType type) throws Exception {
         Gson gson = new Gson();
         if(type == NodeType.MINER) {
-            getServerThread().start();
             System.out.println("selected type: miner");
-            NetworkManager manager = new NetworkManager();
+            // NetworkManager manager = new NetworkManager();
             System.out.println("connecting to other people");
             loadBlockChain();
             /**
              * Later on in the class, i define a method that asks other nodes to send their blockchain starting after a block height of what i currently have.
              */
             if(blockChain.size() == 0) {
+                peerServer.init();
+                peerLoader.init();
+                for(Peer p : peerLoader.peers) {
+                    BeamMessage message = new BeamMessage();
+                    message.set("event","nodejoin");
+                    message.set("address", getIp());
+                    message.set("port","45800");
+                    System.out.println("sending: " + getIp());
+                    p.socket.queueMessage(message);
+                }
                 System.out.println("Blockchain empty.");
                 Block genesis = FunnycoinCache.getCurrentBlock();
                 genesis.transactions.add(new Transaction("coinbase",wallet.getBase64Key(wallet.publicKey),50.0f,"null"));
-                genesis.mine(getDifficulty());
-                blockChain.add(genesis);
-                syncBlockchainFile();
-                /**
-                 * We have set the local blockchain since we know our own block is valid!
-                 * we are going to send the block to other people now.
-                 */
-                JsonObject object = new JsonObject();
-                object.addProperty("event","newBlock");
-                object.addProperty("block",gson.toJson(genesis));
-                while(true) {
-                    mine();
-                    peerServer.broadcast("HALO");
+                if(genesis.mine(getDifficulty())) {
+                    blockChain.add(genesis);
+                    syncBlockchainFile();
+                    /**
+                     * We have set the local blockchain since we know our own block is valid at genesis. who else would make a fraudulent block when they don't know the chain exists;
+                     * we are going to send the block to other people now;
+                     */
+                    JsonObject object = new JsonObject();
+                    object.addProperty("event", "newBlock");
+                    object.addProperty("block", gson.toJson(genesis));
+                    while (true) {
+                        mine();
+                    }
                 }
             } else {
-                getBlocksAfter(getCurrentBlock().height);
+                peerServer.init();
+                peerLoader.init();
+                for(Peer p : peerLoader.peers) {
+                    BeamMessage message = new BeamMessage();
+                    message.set("event","nodejoin");
+                    message.set("address", getIp());
+                    message.set("port","45800");
+                    p.socket.queueMessage(message);
+                }
+
+                // getBlocksAfter(getCurrentBlock().height);
                 while(true) {
                     mine();
-                    peerServer.broadcast("HALO");
                 }
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         } else if(type == NodeType.WALLET) {
             FunnycoinCache.loadBlockChain();
             System.out.print(getBalanceFromChain(wallet.getBase64Key(wallet.publicKey)));
-            NetworkManager manager = new NetworkManager();
-            Wallet myWallet = new Wallet();
+            peerLoader.init();
             while(true) {
-                Scanner scanner = new Scanner(System.in);
-                String cmd = scanner.nextLine();
-                String[] args = cmd.split(" ");
-                for(String arg : args) {
-                    System.out.println(arg);
-                }
-                String command = args[0];
-
-                if(command.toLowerCase().contains("send")) {
-                    String ownerKey = myWallet.getBase64Key(myWallet.publicKey);
-                    String outputKey = args[1];
-                    float amount = Float.parseFloat(args[2]);
-
-                    /**
-                     * Setup transaction json for sending to remote nodes :)
-                     */
-                    JsonObject transactionData = new JsonObject();
-                    transactionData.addProperty("event","transaction");
-                    transactionData.addProperty("ownerKey",ownerKey);
-                    transactionData.addProperty("recipientKey",outputKey);
-                    transactionData.addProperty("amount",amount);
-                    // creating signature now hold up lemme find the method
-                    String signatureData = SignageUtils.sign(SignageUtils.applySha256(ownerKey + outputKey + amount), myWallet.privateKey);
-                    transactionData.addProperty("signatureData",signatureData);
-                    manager.broadcast(transactionData.toString());
-                }
-
+                Thread.sleep(1000);
             }
         }
     }
@@ -159,9 +125,6 @@ public class Funnycoin {
 
     public static void main(String[] args) throws Exception {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-        for(String arg : args) {
-            System.out.println(arg);
-        }
         if(args.length == 1) {
             if(args[0].toLowerCase().contains("miner")) {
                 new Funnycoin(NodeType.MINER);
@@ -177,7 +140,6 @@ public class Funnycoin {
 
     public enum NodeType {
         MINER,
-        VOTER,
         WALLET
     }
 }
