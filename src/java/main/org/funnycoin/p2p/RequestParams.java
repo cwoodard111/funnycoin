21e82b540531e29a0e2912cf6f77b9accbb7e6dc
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-04-07 17:38:39 -0400

    Update RequestParams.java

--- src/java/main/org/funnycoin/p2p/RequestParams.java
+++ src/java/main/org/funnycoin/p2p/RequestParams.java
@@ -2,8 +2,6 @@ package org.funnycoin.p2p;
 
 public class RequestParams {
 
-    public static int blockHeight = 0;
-
-    public static boolean requestingBlocks = false;
+    public static boolean interrupted = false;
 
 }
