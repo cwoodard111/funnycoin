f2559ce65ab97be083c7cdd7c8c6924e5553e1e2
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-04-07 17:40:30 -0400

    Update VerificationUtils.java

--- src/java/main/org/funnycoin/miner/VerificationUtils.java
+++ src/java/main/org/funnycoin/miner/VerificationUtils.java
@@ -1,4 +1,7 @@
 package org.funnycoin.miner;
+/**
+ * Trust me there will be a reason for it being in a different package in the future, trust me.
+ */
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -8,6 +11,14 @@ import java.util.Base64;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class VerificationUtils {
+    /**
+     **
+     * @param plainText
+     * @param signature
+     * @param publicKey
+     * @return boolean
+     * @throws Exception
+     */
     public static boolean verify(String plainText, String signature, PublicKey publicKey) throws Exception {
         Signature publicSignature = Signature.getInstance("RSA");
         publicSignature.initVerify(publicKey);
