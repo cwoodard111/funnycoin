5cade97b4bbc9733b88d65958301f744aa8447ba
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-04-12 11:02:49 -0400

    Update PeerLoader.java

--- src/java/main/org/funnycoin/p2p/PeerLoader.java
+++ src/java/main/org/funnycoin/p2p/PeerLoader.java
@@ -35,26 +35,30 @@ public class PeerLoader {
             e.printStackTrace();
         }
         Peer[] peersArray = new Gson().fromJson(peerBuilder.toString(),Peer[].class);
-        for(Peer p : peersArray) {
-            System.out.println(p.address + " null");
-            if(p.peerIsOnline()) {
-                try {
-                    if (!(p.address.contains(FunnycoinCache.getIp()))) {
-                        peers.add(p);
-                        p.connectToPeer();
+        if(peerBuilder.toString().length() > 1) {
+            for (int k = 0; k < peersArray.length; k++) {
+                Peer p = peersArray[k];
+                if (p.peerIsOnline()) {
+                    try {
+                        System.out.println(p.address + ":" + p.port);
+                            System.out.println("connecting");
+                            peers.add(p);
+                            p.connectToPeer();
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                } catch (Exception e ) {
-                    e.printStackTrace();
                 }
             }
         }
         System.out.println("SIZE: " + peers.size());
         if(peers.size() == 0) {
+            System.out.println("we dont have any peers unfortunately, gonna wait till someone tries to connect to someone i guess.");
             while (peers.size() == 0) {
-                for (Peer p : peersArray) {
+                for (int k = 0; k < peers.size(); k++) {
+                    Peer p = peers.get(k);
                     if (p.peerIsOnline()) {
                         try {
-                            if (!(p.address.contains(FunnycoinCache.getIp()))) {
+                            if ((p.port != FunnycoinCache.peerServer.port)) {
                                 peers.add(p);
                                 p.connectToPeer();
                             }
@@ -64,6 +68,14 @@ public class PeerLoader {
                     }
                 }
             }
+        } else {
+            System.out.println("peers added successfully!");
         }
     }
+    public void removeDuplicates() {
+        for(Peer p : peers) {
+            peers.removeIf(j -> j.address == p.address);
+        }
+    }
+
 }
