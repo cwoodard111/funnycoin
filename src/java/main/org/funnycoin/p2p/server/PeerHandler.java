4bda93b528e4790932f0ab6c593429bee597c92a
itzblaze111 <37526592+itzblaze111@users.noreply.github.com>
2021-04-12 11:03:20 -0400

    Update PeerHandler.java

--- src/java/main/org/funnycoin/p2p/server/PeerHandler.java
+++ src/java/main/org/funnycoin/p2p/server/PeerHandler.java
@@ -6,6 +6,7 @@ import com.codebrig.beam.messages.BasicMessage;
 import com.codebrig.beam.messages.BeamMessage;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import org.funnycoin.Funnycoin;
 import org.funnycoin.FunnycoinCache;
 import org.funnycoin.blocks.Block;
 import org.funnycoin.p2p.Peer;
@@ -30,7 +31,7 @@ public class PeerHandler extends BeamHandler {
         String msg = beamMessage.get("message");
         Gson gson = new Gson();
         if(beamMessage.get("event").toLowerCase().contains("newblock")) {
-            List<Block> tempChain = new ArrayList<>();
+            List<Block> tempChain = new ArrayList<>(FunnycoinCache.blockChain);
             tempChain.add(gson.fromJson(msg,Block.class));
             try {
                 if(isChainValid(tempChain)) {
@@ -38,10 +39,12 @@ public class PeerHandler extends BeamHandler {
                     System.out.println("chain is valid.");
                     FunnycoinCache.blockChain = tempChain;
                     FunnycoinCache.syncBlockchainFile();
+                    System.out.println("synced blockchain file");
                 } else {
                     System.out.println("the chain is not valid");
                 }
             } catch (Exception e) {
+                System.out.println("EXCEPTION BRO");
                 e.printStackTrace();
             }
         } else if(beamMessage.get("event").toLowerCase().contains("nodejoin")) {
@@ -49,12 +52,14 @@ public class PeerHandler extends BeamHandler {
             String address = beamMessage.get("address");
             int port = Integer.parseInt(beamMessage.get("port"));
             System.out.println(address + ":" + port);
-            Peer p = new Peer(address,port);
-            for(Peer j : FunnycoinCache.peerLoader.peers) {
-                if(!j.address.equals(p.address) && p.port != j.port) {
-                    p.connectToPeer();
-                    FunnycoinCache.peerLoader.peers.add(p);
+            Peer p = new Peer(address, port);
+            if (FunnycoinCache.peerLoader.peers.size() < 1) {
+                for(Peer peer : FunnycoinCache.peerLoader.peers) {
+                    if (!(peer.address == p.address && peer.port == p.port)) {
+                    }
                     try {
+                        p.connectToPeer();
+                        FunnycoinCache.peerLoader.peers.add(p);
                         FunnycoinCache.peerLoader.syncPeerFile();
                         System.out.println("added peer to peers.");
                     } catch (IOException e) {
@@ -62,15 +67,34 @@ public class PeerHandler extends BeamHandler {
                         System.err.println("couldn't write to peer file for some reason:" + e.getMessage());
                     }
                 }
+            } else {
+                for (int k = 0; k < FunnycoinCache.peerLoader.peers.size(); k++) {
+                    Peer j = FunnycoinCache.peerLoader.peers.get(k);
+                    if (p.port != j.port) {
+                        try {
+                            FunnycoinCache.peerLoader.peers.add(p);
+                            FunnycoinCache.peerLoader.syncPeerFile();
+                            System.out.println("added peer to peers.");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            System.err.println("couldn't write to peer file for some reason:" + e.getMessage());
+                        }
+                        FunnycoinCache.peerLoader.peers.get(FunnycoinCache.peerLoader.peers.size()).connectToPeer();
+                    }
+                }
             }
+            FunnycoinCache.peerLoader.removeDuplicates();
         }
-        return null;
+        return beamMessage.emptyResponse();
     }
+
+
     private boolean isChainValid(List<Block> blockChain) throws Exception {
+        boolean valid = false;
         for(int i = 0; i < blockChain.size(); i++) {
-            String difficultyTarget = new String(new char[FunnycoinCache.getDifficulty()]).replace('\0','0');
             Block currentBlock = blockChain.get(i);
-            if(blockChain.size() > 1) {
+            String difficultyTarget = new String(new char[FunnycoinCache.getBlockDifficulty(i)]).replace('\0','0');
+            if(blockChain.size() > 1 && i != 0) {
                 Block previousBlock = blockChain.get(i - 1);
                 /**
                  * Performing checks on the temporary blockchain to see if it's valid.
@@ -83,7 +107,7 @@ public class PeerHandler extends BeamHandler {
                     System.out.println("The hash of the previous block is not equal to the calculated value.");
                     return false;
                 }
-                if(!currentBlock.hash.substring(0,FunnycoinCache.getDifficulty()).equals(difficultyTarget)) {
+                if(!currentBlock.hash.substring(0,FunnycoinCache.getDifficulty).equals(difficultyTarget)) {
                     System.out.println("The block does not have a Proof-Of-Work attached to it.");
                     return false;
                 }
@@ -92,11 +116,13 @@ public class PeerHandler extends BeamHandler {
                  */
                 for(int j = 0; j < currentBlock.getTransactions().size(); j++) {
                     Transaction currentTransaction = currentBlock.transactions.get(j);
-
+                    System.out.println("Transaction sender,reciever:" + currentTransaction);
+                    if(currentTransaction.getOwnerKey().toLowerCase().equals("coinbase")) {
+                        valid = true;
+                        continue;
+                    }
                     if(!currentTransaction.verify(currentTransaction.getHash(),currentTransaction.signature, SignageUtils.getPublicKey(currentTransaction.getOwnerKey()))) {
-                        if(currentTransaction.getOwnerKey().toLowerCase().equals("coinbase")) {
-                            return true;
-                        }
+                        return false;
                     }
                 }
 
@@ -105,9 +131,9 @@ public class PeerHandler extends BeamHandler {
                 /**
                  * We are going to just say it's correct because it's at genesis.
                  */
-                return true;
+                valid = true;
             }
         }
-        return false;
+        return valid;
     }
 }
